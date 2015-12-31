47486886d6a5759da881657b5c5122148ddf56c8
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2015-12-31 11:47:24 -0700

    a bit more progress on the auto-registration scripts

--- scripts/nodestartup.py
+++ scripts/nodestartup.py
@@ -2,7 +2,7 @@
 
 #python script that registers a node with the pi home automation server so that the server knows that its there
 #should be called at Pi (or other mini-computer) startup
-#usage: ./nodestartup.py [docussandra-url]
+#usage: ./nodestartup.py [docussandra-url] [node-name]
 #author: Jeffrey DeYoung
 #create date: 30 Dec 15
 
@@ -13,7 +13,12 @@ import sys
 
 def register():
     print "Node startup script started. Using server endpoint: " + sys.argv[1]
-    myResponse = requests.get(url, verify=True)
+    base_url = sys.argv[1] + "/pihomeautomation/sensor_nodes"
+    query_url = base_url + "/queries"
+    where = "{\"where\":\"name = '"+sys.argv[2]+"'\"}"
+    print "Posting: " + where + " to: " + query_url
+    myResponse = requests.post(query_url, data=where)
+    print myResponse
 
 
 #run the register method by default
